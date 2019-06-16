0f7fcedbac565a5d54ce325c91d3960e0051d123
Konstantin <konstantin.v.modin@gmail.com>
2019-06-16 07:14:08 +0200

    updates

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import Frag from '../HOC/Frag';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
     
-    
+    deleteHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then((response) => {
+                console.log('response', response);
+            });
+    }
     
     render () {
+        console.log(this.props.post);
         const post = !this.props.id ? <p>Please select a Post!</p> :
             <Frag>
                 <h1>{this.props.post.title}</h1>
                 <p>{this.props.post.body}</p>
                 <div className="Edit">
-                    <button className="Delete">Delete</button>
+                    <button className="Delete" onClick={this.deleteHandler}>Delete</button>
                 </div>
             </Frag>;
        
--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -7,6 +8,18 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        };
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
@@ -19,10 +32,11 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                    <option value="Konst">Konst</option>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
