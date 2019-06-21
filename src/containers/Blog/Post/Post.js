5571a546912b6e1a63afdade68740185f9c1a308
Konstantin <konstantin.v.modin@gmail.com>
2019-06-21 15:25:33 +0200

    updates

--- src/containers/Blog/Post/Post.js
+++ src/containers/Blog/Post/Post.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { withRouter} from 'react-router-dom';
 
 import './Post.css';
 
@@ -15,4 +14,4 @@ const Post = ({ title, author: a, clicked }) => (
     </article>
 );
 
-export default withRouter(Post);
+export default Post;
