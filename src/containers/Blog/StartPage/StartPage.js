8f226da147d669e3368e55542e56f6ac85ef2dd4
Konstantin <konstantin.v.modin@gmail.com>
2019-06-23 10:10:10 +0200

    updates

--- src/containers/Blog/StartPage/StartPage.js
+++ src/containers/Blog/StartPage/StartPage.js
@@ -4,9 +4,7 @@ import './StartPage.css';
 const StartPage = () => {
     return (
         <div className="StartPage">
-            <h1>Start Page!</h1>   
-            <p className="Post Info Author">P tag</p>
-            <div className="Post Info Author">normal div</div>                  
+            <h1>Start Page!</h1>                      
         </div>
     )
 }
